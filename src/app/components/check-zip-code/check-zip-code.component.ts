1b65f4d66c4003e65ed6a8074b3dc53f980a0312
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-10-03 11:35:14 -0300

    ✨ feat: component creation check-zip-code

---
+++ src/app/components/check-zip-code/check-zip-code.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sa-check-zip-code',
+  templateUrl: './check-zip-code.component.html',
+  styles: [
+    `
+      a {
+        color: var(--details-color);
+        font-size: 17px;
+        margin: 10px 10px 0 0;
+        text-decoration: none;
+      }
+    `,
+  ],
+})
+export class CheckZipCodeComponent {}
