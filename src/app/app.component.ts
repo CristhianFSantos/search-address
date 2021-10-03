e5aecfc790d59e7efede49d7be9ccfc1f091934a
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-10-03 11:42:35 -0300

    ✨ feat: organization and structuring of custom components in the main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { Address } from './models/address';
 
 @Component({
   selector: 'sa-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'search-address';
+
+  currentAddress: Address;
+
+  updateAddress(address: Address) {
+    this.currentAddress = address;
+  }
 }
+/*
+O método updateAddress atualiza a propriedade currentAddress com o address recebido como parâmetro.
+Esse address é o retorno da API que está sendo enviado com o output e está sendo passado na propriedade do componente de resultado*/
