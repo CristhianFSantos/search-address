917b58f563110986000ff14d4adb3b1a4de3abc7
CristhianFSantos <cristhianfelipe0@gmail.com>
2021-10-03 11:36:57 -0300

    ✨ feat: interface creation address

---
+++ src/app/models/address.ts
@@ -0,0 +1,14 @@
+export interface Address {
+  cep: string;
+  address_type: string;
+  address_name: string;
+  address: string;
+  district: string;
+  state: string;
+  city: string;
+  lat: string;
+  lng: string;
+  ddd: string;
+  city_ibge: string;
+}
+/*Interface criada inicialmente para servir de template (modelo) para os dados retornados pela API*/
